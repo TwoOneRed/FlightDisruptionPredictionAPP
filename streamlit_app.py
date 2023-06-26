c8cca3eb5b6db82c96ae11a9e0f37b5c729bae3f
TwoOneRed <leong_2001@hotmail.com>
2023-06-26 11:05:21 +0800

    EEEE

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import pandas as pd
 
-from powerbiclient import Report
 
 st.title('Flight Disruption Prediction Applications')
 st.text('1191100292  Leong Yi Hong')
 
 # Display the report
-st.markdown("https://app.powerbi.com/view?r=eyJrIjoiMmU3ZjFmODItMDE1My00ZDE4LWJhNmQtOTFiYmM1ODAxYWU4IiwidCI6IjdlMGI1ZmNmLTEyYzQtNGVmZi05NmI2LTQ2NjRmMjVkYzdkYSIsImMiOjEwfQ%3D%3D",unsafe_allow_html=True)
+st.markdown("""<iframe title="PB" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMmU3ZjFmODItMDE1My00ZDE4LWJhNmQtOTFiYmM1ODAxYWU4IiwidCI6IjdlMGI1ZmNmLTEyYzQtNGVmZi05NmI2LTQ2NjRmMjVkYzdkYSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>""",unsafe_allow_html=True)
