b37016aab9cb0a84f1297e156f0864f041bc9219
TwoOneRed <leong_2001@hotmail.com>
2023-06-26 10:48:32 +0800

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,28 @@
 import streamlit as st
 import pandas as pd
 
+from powerbiclient import Report
+from embedpy.powerbi import Report
+
 st.title('Flight Disruption Prediction Applications')
 st.text('1191100292  Leong Yi Hong')
+
+
+# Define your Azure AD and Power BI details
+tenant_id = '7419ae17-5bc9-4dde-a96b-50878d8575f4'
+client_id = 'ad3b6b53-2382-418f-8014-13aeaf79205e'
+client_secret = 'kxc8Q~UJpDQZEHdVOrVaJBH0He5rhvWgwFhHLbKK'
+authority = f"https://login.microsoftonline.com/{tenant_id}"
+
+group_id = "f4b32c96-7686-4187-ba83-59ad83d16377"
+report_id = 'ff3647ff-cd01-4251-b793-64f480078853'  # Replace with your actual report ID
+
+# Create an instance of the Report class
+report = Report(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret, authority=authority, report_id=report_id, group_id=group_id)
+
+# Create a Streamlit app
+st.title("Power BI Dashboard")
+
+# Display the report
+report_url = report.get_embed_url()
+st.components.v1.iframe(report_url, height=600)
