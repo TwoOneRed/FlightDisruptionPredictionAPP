5af16cc7a7f26a10dff7beabb65e422d817919c4
TwoOneRed <leong_2001@hotmail.com>
2023-06-26 16:27:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ df = pd.DataFrame()
 if uploaded_file:
     # Do something with the uploaded file
     df = pd.read_csv(uploaded_file)
+    st.text("Uploaded Dataset")
     st.dataframe(df.head(5))
     sheet = client.open("FYP2Data_PowerBI").sheet1
     # Clear the existing data from the sheet
@@ -32,6 +33,11 @@ if uploaded_file:
 
     # Update the Google Sheets file with the modified DataFrame
     st.write(sheet.update([newdata.columns.values.tolist()] + newdata.values.tolist()))
+
+else:
+    st.text("Original Dataset")
+    sheet = client.open("FYP2Data_PowerBI").sheet1
+    df = pd.DataFrame(sheet)
 
 # Create a Streamlit app
 st.title("Power BI Dashboard")
@@ -51,81 +57,81 @@ def model(label,X,y):
     y_pred = loaded_model.predict(X)
     model.append("Naive Bayes")
     accuracy.append(round((accuracy_score(y, y_pred)*100), 2))
-    precision.append(precision_score(y, y_pred))
-    recall.append(recall_score(y, y_pred))
-    f1.append(f1_score(y, y_pred))
+    precision.append(precision_score(y, y_pred, zero_division=0, average='weighted'))
+    recall.append(recall_score(y, y_pred, average='weighted'))
+    f1.append(f1_score(y, y_pred, average='weighted'))
 
     filename = "Model/"+label+"_svm.pkl"
     loaded_model = pickle.load(open(filename,'rb'))
     y_pred = loaded_model.predict(X)
     model.append("Support Vector Machine")
     accuracy.append(round((accuracy_score(y, y_pred)*100), 2))
-    precision.append(precision_score(y, y_pred))
-    recall.append(recall_score(y, y_pred))
-    f1.append(f1_score(y, y_pred))
+    precision.append(precision_score(y, y_pred, zero_division=0, average='weighted'))
+    recall.append(recall_score(y, y_pred, average='weighted'))
+    f1.append(f1_score(y, y_pred, average='weighted'))
 
     filename = "Model/"+label+"_dt.pkl"
     loaded_model = pickle.load(open(filename,'rb'))
     y_pred = loaded_model.predict(X)
     model.append("Decision Tree")
     accuracy.append(round((accuracy_score(y, y_pred)*100), 2))
-    precision.append(precision_score(y, y_pred))
-    recall.append(recall_score(y, y_pred))
-    f1.append(f1_score(y, y_pred))
+    precision.append(precision_score(y, y_pred, zero_division=0, average='weighted'))
+    recall.append(recall_score(y, y_pred, average='weighted'))
+    f1.append(f1_score(y, y_pred, average='weighted'))
 
     filename = "Model/"+label+"_rf.pkl"
     loaded_model = pickle.load(open(filename,'rb'))
     y_pred = loaded_model.predict(X)
     model.append("Random Forest")
     accuracy.append(round((accuracy_score(y, y_pred)*100), 2))
-    precision.append(precision_score(y, y_pred))
-    recall.append(recall_score(y, y_pred))
-    f1.append(f1_score(y, y_pred))
+    precision.append(precision_score(y, y_pred, zero_division=0, average='weighted'))
+    recall.append(recall_score(y, y_pred, average='weighted'))
+    f1.append(f1_score(y, y_pred, average='weighted'))
 
     filename = "Model/"+label+"_knn.pkl"
     loaded_model = pickle.load(open(filename,'rb'))
     y_pred = loaded_model.predict(X)
     model.append("K Nearest Neighbours")
     accuracy.append(round((accuracy_score(y, y_pred)*100), 2))
-    precision.append(precision_score(y, y_pred))
-    recall.append(recall_score(y, y_pred))
-    f1.append(f1_score(y, y_pred))
+    precision.append(precision_score(y, y_pred, zero_division=0, average='weighted'))
+    recall.append(recall_score(y, y_pred, average='weighted'))
+    f1.append(f1_score(y, y_pred, average='weighted'))
 
     filename = "Model/"+label+"_lr.pkl"
     loaded_model = pickle.load(open(filename,'rb'))
     y_pred = loaded_model.predict(X)
     model.append("Logistic Regression")
     accuracy.append(round((accuracy_score(y, y_pred)*100), 2))
-    precision.append(precision_score(y, y_pred))
-    recall.append(recall_score(y, y_pred))
-    f1.append(f1_score(y, y_pred))
+    precision.append(precision_score(y, y_pred, zero_division=0, average='weighted'))
+    recall.append(recall_score(y, y_pred, average='weighted'))
+    f1.append(f1_score(y, y_pred, average='weighted'))
+
 
     return pd.DataFrame({"Model":model,"Accuracy":accuracy,"Precision":precision,"Recall":recall,"F1-score":f1})
 
 
-if uploaded_file:
-    rfe_score=pd.read_csv("Model/rfe_score.csv")
+rfe_score=pd.read_csv("Model/rfe_score.csv")
 
-    df = df.drop(columns=['dep_Lat','dep_Lon','arr_Lat','arr_Lon','delayed'])
-    data_encode = df.copy()
-    data_encode = data_encode.apply(LabelEncoder().fit_transform)
+df = df.drop(columns=['dep_Lat','dep_Lon','arr_Lat','arr_Lon','delayed'])
+data_encode = df.copy()
+data_encode = data_encode.apply(LabelEncoder().fit_transform)
 
-    X = data_encode.drop('delayStatus', axis = 1)
-    X10 = X[rfe_score.Features[0:10]].copy()
-    X30 = X[rfe_score.Features[0:30]].copy()
-    y = data_encode['delayStatus']
+X = data_encode.drop('delayStatus', axis = 1)
+X10 = X[rfe_score.Features[0:10]].copy()
+X30 = X[rfe_score.Features[0:30]].copy()
+y = data_encode['delayStatus']
 
-    st.title("Prediction Result Using Pre-Trained Model")
-    st.text("Model Trained Using top 10 Features and Normal Data")
-    st.dataframe(model("norm10",X10,y))
+st.title("Prediction Result Using Pre-Trained Model")
+st.text("Model Trained Using top 10 Features and Normal Data")
+st.dataframe(model("norm10",X10,y))
 
-    st.text("Model Trained Using top 30 Features and Normal Data")
-    st.dataframe(model("norm30",X30,y))
+st.text("Model Trained Using top 30 Features and Normal Data")
+st.dataframe(model("norm30",X30,y))
 
-    st.text("Model Trained Using top 10 Features and SMOTE Data")
-    st.dataframe(model("smote10",X10,y))
+st.text("Model Trained Using top 10 Features and SMOTE Data")
+st.dataframe(model("smote10",X10,y))
 
-    st.text("Model Trained Using top 30 Features and SMOTE Data")
-    st.dataframe(model("smote30",X30,y))
+st.text("Model Trained Using top 30 Features and SMOTE Data")
+st.dataframe(model("smote30",X30,y))
 
 
